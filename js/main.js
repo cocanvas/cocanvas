6755bcd63482e7bb7572b82321162bde22478072
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-20 15:21:14 +1000

    grid is up and working with hover yay

--- js/main.js
+++ js/main.js
@@ -1,55 +1,68 @@
-let context = canvas.getContext('2d'),
-let columns = 80;
-let rows = 60;
-let w
-h, tileWidth, tileHeight;
+$(document).ready( function () {
+  let ctx = canvas.getContext('2d');
+  let columns = 80;
+  let rows = 60;
+  // let w = '600px';
+  // let h = '600px';
+  let tileWidth;
+  let tileHeight;
 
-canvas.onresize = calcSize;
-canvas.onmousemove = highlight;
+  // canvas.onresize = calcSize;
+  canvas.onmousemove = highlight;
 
-calcSize();
+  canvas.width = w = 802;
+  canvas.height = h = 602;
+  tileWidth = w / columns;
+  tileHeight = h / rows;
+  ctx.strokeStyle = '#a1a1a1';
+  ctx.fillStyle = '#f70';
+  render();
 
-function calcSize() {
-    canvas.width = w = window.innerWidth;
-    canvas.height = h = window.innerHeight;
+  // calcSize();
+  //
+  // function calcSize() {
+  //     // canvas.width = w;
+  //     // canvas.height = h;
+  //
+  //
+  //
+  //
+  //     render();
+  // }
 
-    tileWidth = w / columns;
-    tileHeight = h / rows;
+  function render() {
 
-    ctx.strokeStyle = '#000';
-    ctx.fillStyle = '#f70';
+      ctx.clearRect(0, 0, w, h);
 
-    render();
-}
-function render() {
+      ctx.beginPath();
 
-    ctx.clearRect(0, 0, w, h);
+      for(let x = 0; x < columns; x++) {
+          ctx.moveTo(x * tileWidth, 0);
+          ctx.lineTo(x * tileWidth, h);
+      }
+      for(var y = 0; y < rows; y++) {
+          ctx.moveTo(0, y * tileHeight);
+          ctx.lineTo(w, y * tileHeight);
+      }
+      ctx.stroke();
+  }
 
-    ctx.beginPath();
+  function highlight(e) {
 
-    for(var x = 0; x < columns; x++) {
-        ctx.moveTo(x * tileWidth, 0);
-        ctx.lineTo(x * tileWidth, h);
-    }
-    for(var y = 0; y < rows; y++) {
-        ctx.moveTo(0, y * tileHeight);
-        ctx.lineTo(w, y * tileHeight);
-    }
-    ctx.stroke();
-}
-function highlight(e) {
+      var rect = canvas.getBoundingClientRect(),
+          mx = e.clientX - rect.left,
+          my = e.clientY - rect.top,
 
-    var rect = canvas.getBoundingClientRect(),
-        mx = e.clientX - rect.left,
-        my = e.clientY - rect.top,
+          /// get index from mouse position
+          xIndex = Math.round((mx - tileWidth * 0.5) / tileWidth),
+          yIndex = Math.round((my - tileHeight * 0.5) / tileHeight);
 
-        /// get index from mouse position
-        xIndex = Math.round((mx - tileWidth * 0.5) / tileWidth),
-        yIndex = Math.round((my - tileHeight * 0.5) / tileHeight);
+      render();
 
-    render();
+      ctx.fillRect(xIndex * tileWidth,
+                   yIndex * tileHeight,
+                   tileWidth,
+                   tileHeight);
+  }
 
-    ctx.fillRect(xIndex * tileWidth,
-                 yIndex * tileHeight,
-                 tileWidth,
-                 tileHeight);
+});
