c8ca7a224f1e8bf96c4d4d66e8f18087566e9987
robo2323 <rob.adam.h@gmail.com>
2018-04-25 15:28:09 +1000

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: ['/node_modules/','/public/']
       },
       {
         test: /\.s?css$/,
