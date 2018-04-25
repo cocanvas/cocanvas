5d45643f1e7b22e7a1bdf0f25423cd886abd8ed4
robo2323 <rob.adam.h@gmail.com>
2018-04-25 15:32:08 +1000

    rm bundle

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         test: /\.js$/,
-        exclude: ['/node_modules/','/public/']
+        exclude: [/node_modules/,'/public/']
       },
       {
         test: /\.s?css$/,
