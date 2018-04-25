5845a53d9269a4ef31184dae537df4e22736af73
robo2323 <rob.adam.h@gmail.com>
2018-04-25 16:10:43 +1000

    webpack, build optimized

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 // webpack config requires entry point and output
 module.exports = {
   entry: ['babel-polyfill', './src/main.js'],
@@ -6,6 +9,18 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+      threshold: 10240,
+      minRatio: 0.8
+  })
+],
   module: {
     rules: [
       {
