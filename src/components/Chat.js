93ad323069475795755be99de7e0395ccaba8aef
robo2323 <rob.adam.h@gmail.com>
2018-04-26 11:14:59 +1000

    message time format

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -81,7 +81,7 @@ class Chat extends Component {
         <div key={`chat_${el.id}`}>
           <p>
             <span className="chat-user">{el.username} </span>
-            <span className="chat-created-at">{`${new Date(el.created_at).getHours()}:${new Date(el.created_at).getMinutes()}`}</span>
+            <span className="chat-created-at">{ `${new Date(el.created_at).getHours()}:${new Date(el.created_at).getMinutes()}`}</span>
           </p>
           <p>
             <span className="chat-message">{el.content}</span>
